10f6f172dcc931fd738f0141da06e89fb4a2ac19
Andreas Bielawski <brawl98@outlook.com>
2019-03-06 18:30:51 +0100

    Support net.bin

--- bscu.py
+++ bscu.py
@@ -27,8 +27,26 @@ class SaveFile:
             return self.pack() + self.calculate_checksum()
 
         def __str__(self):
-            output = "AutoSave\n"
+            output = "AutoSave:\n"
             output += "  Golden Hammers: {0}\n".format(self.goldenHammers)
+            output += "  Checksum: {0}\n".format(binascii.hexlify(self.calculate_checksum()).decode())
+
+            return output
+
+    class Net(Struct):
+        __endian__ = Struct.BE
+
+        def __format__(self):
+            self.unknown = Struct.string(0x281C)
+
+        def calculate_checksum(self):
+            return binascii.crc32(self.pack()).to_bytes(4, "big")
+
+        def pack_with_checksum(self):
+            return self.pack() + self.calculate_checksum()
+
+        def __str__(self):
+            output = "Net:\n"
             output += "  Checksum: {0}\n".format(binascii.hexlify(self.calculate_checksum()).decode())
 
             return output
@@ -36,11 +54,17 @@ class SaveFile:
     def __init__(self, directory):
         self.directory = directory
         autosave_file = []
+        net_file = []
         for i in range(2):
             try:
                 autosave_file.append(open(os.path.join(self.directory, "autosv{0}.bin".format(i)), "rb").read())
             except FileNotFoundError:
                 raise FileNotFoundError("autosv{0}.bin not found".format(i))
+
+            try:
+                net_file.append(open(os.path.join(self.directory, "net{0}.bin".format(i)), "rb").read())
+            except FileNotFoundError:
+                raise FileNotFoundError("net{0}.bin not found".format(i))
 
         self.autosave = []
         for num, autosave in enumerate(autosave_file):
@@ -53,6 +77,20 @@ class SaveFile:
 
         if autosave_file[0] != autosave_file[1]:
             print("WARNING: autosv0.bin is NOT THE SAME as autosv1.bin!")
+
+        self.net = []
+        for num, net in enumerate(net_file):
+            if len(net) != 10272:
+                raise Exception("This is not a valid net{0}.bin!".format(num))
+            self.net.append(self.Net().unpack(net))
+            self.net[num].checksum = net[-4:]
+            if self.net[num].calculate_checksum() != self.net[num].checksum:
+                print(self.net[num].calculate_checksum())
+                print(self.net[num].checksum)
+                print("WARNING: net{0}.bin CRC32 checksum mismatch!".format(num))
+
+        if net_file[0] != net_file[1]:
+            print("WARNING: net0.bin is NOT THE SAME as net1.bin!")
 
     def set_golden_hammers(self, num):
         """Sets golden hammers from 0 to 255.
@@ -70,6 +108,15 @@ class SaveFile:
             with open(os.path.join(self.directory, "autosv{0}.bin".format(num)), "wb") as autosave_file:
                 autosave_file.write(autosave.pack_with_checksum())
 
+    def update_net_checksum(self):
+        for num, net in enumerate(self.net):
+            with open(os.path.join(self.directory, "net{0}.bin".format(num)), "wb") as net_file:
+                net_file.write(net.pack_with_checksum())
+
+    def update_checksums(self):
+        self.update_autosave_checksum()
+        self.update_net_checksum()
+
     def __str__(self):
         output = "Super Smash Bros. Brawl Savefile\n"
         for num, autosave in enumerate(self.autosave):
@@ -81,5 +128,7 @@ class SaveFile:
 
         output += "\n"
         output += str(self.autosave[0])
+        output += "\n"
+        output += str(self.net[0])
 
         return output
